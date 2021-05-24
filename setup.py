626a24ff5a5d8bc0794686449642c3a443001a33
Marcin Magnus <mag_dex@o2.pl>
2021-05-24 16:49:06 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import versioneer
+
+# read the contents of your README file
+with open('README.md') as f:
+    long_description = f.read()
+    
+setup(
+    name='spotifier',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    test_suite="tests",
+    url='https://github.com/mmagnus/yeast-spotifier',
+    scripts=['spotifier.py',
+             'digitifier.py',             
+             ],
+    license='GPLv3',
+    author='Marcin Magnus',
+    author_email='mag_dex@o2.pl',
+    description='a script to process yeast plate image into figures',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=[
+       ],
+)
