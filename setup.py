823476692a31eab024b8509fa4d6a59ca664bde8
Marcin Magnus <mag_dex@o2.pl>
2021-08-10 18:24:52 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
     long_description = f.read()
     
 setup(
-    name='spotifier',
+    name='yeast-spotifier',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(),
@@ -23,5 +23,7 @@ setup(
     long_description_content_type="text/markdown",
     include_package_data=True,
     install_requires=[
+        'pillow',
+        'psd-tools3',
        ],
 )
